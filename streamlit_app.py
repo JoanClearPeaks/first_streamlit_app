df13158ebe67af1b77f167e6eac92324faaf00a4
JoanClearPeaks <138578111+JoanClearPeaks@users.noreply.github.com>
2023-07-26 12:42:21 +0200

    Update streamlit_app.py
    api

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,10 @@ streamlit.dataframe(frui_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon") #s'agafen dades de watermelon
+streamlit.text(fruityvice_response.json()) #s'escriuen per pantalla les dades
 
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)

847a2fb1552d27ac86fea67a88c379045a857ea1
JoanClearPeaks <138578111+JoanClearPeaks@users.noreply.github.com>
2024-02-10 14:20:20 +0100

    Update outlier.py

--- outlier.py
+++ outlier.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
 # Lee el archivo CSV
-predictive = pd.read_csv('datos.csv')
+predictive = pd.read_csv('predictive_maintenance.csv')
+st.sidebar.subheader('COLUMN', help="Select the target column.")
+target_column = st.sidebar.selectbox("Select the target column", [None] +  list(selection.columns), index=0, label_visibility="collapsed")
+
+

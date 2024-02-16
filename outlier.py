57866008cb3959949b0f56785df40a1efc80cdb6
JoanClearPeaks <138578111+JoanClearPeaks@users.noreply.github.com>
2024-02-16 09:57:47 +0100

    Update outlier.py

--- outlier.py
+++ outlier.py
@@ -43,7 +43,7 @@ class Outlier_Quantiles():
         self.start_date = selection[self.date_column].min()
         self.end_date = selection[self.date_column].max()
         date_range = [date.date() for date in pd.date_range(start=self.start_date, end=self.end_date, freq='D') if date.date() in selection[self.date_column].tolist()]        
-        
+        st.write(date_range)
         st.write("          ")
         st.sidebar.markdown("**Date Range**")
         self.start_date = st.sidebar.selectbox("Select Start Date", [None] + date_range)

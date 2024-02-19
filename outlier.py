d501f8e4ae7c116f9b2c207976fec86e09974620
JoanClearPeaks <138578111+JoanClearPeaks@users.noreply.github.com>
2024-02-19 09:34:36 +0100

    Update outlier.py

--- outlier.py
+++ outlier.py
@@ -56,7 +56,7 @@ class Outlier_Quantiles():
       st.write("          ")
       st.sidebar.markdown("**Date Range**")
       # self.start_date = st.sidebar.selectbox("Select Start Date", [None] + date_range)
-
+      st.write(data_range)
       self.start_date = st.sidebar.date_input(
       "Select Start Date", None,
       min_value=min(date_range),

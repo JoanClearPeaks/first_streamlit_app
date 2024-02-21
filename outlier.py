b86b6f7077b11c816eb24a0b65c78b8827fbc174
JoanClearPeaks <138578111+JoanClearPeaks@users.noreply.github.com>
2024-02-21 10:25:56 +0100

    Update outlier.py

--- outlier.py
+++ outlier.py
@@ -28,7 +28,7 @@ class Outlier_Quantiles():
     for col in selection.columns:
           if selection[col].dtype == 'object':
               try:
-                  selection[col] = pd.to_datetime(selection[col]).dt
+                  selection[col] = pd.to_datetime(selection[col])
               except ValueError:
                   pass
     st.subheader('Dtypes2')

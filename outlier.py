11e6ae2f144a5e753c04712baafe5e302ae41258
JoanClearPeaks <138578111+JoanClearPeaks@users.noreply.github.com>
2024-02-21 09:27:50 +0100

    Update outlier.py

--- outlier.py
+++ outlier.py
@@ -43,6 +43,8 @@ class Outlier_Quantiles():
     self.warning_dic = {}
       
     if self.date_column != 'False':
+      st.write(type(self.date_column))
+      st.write(type(self.date_column).__name__)
       selection[self.date_column] = selection[self.date_column].replace('2019-09-11', '2020-01-11')
       selection[self.date_column] = selection[self.date_column].replace('2019-09-12', '2018-01-11')
       selection[self.date_column] = selection[self.date_column].replace('2019-09-24', '2019-09-23')

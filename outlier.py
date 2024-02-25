4585536e4c7646eadea30eac9ee86aea2c398319
JoanClearPeaks <138578111+JoanClearPeaks@users.noreply.github.com>
2024-02-25 21:04:21 +0100

    Update outlier.py

--- outlier.py
+++ outlier.py
@@ -552,8 +552,19 @@ Select the grouping criterion for observations, where numerical values will be a
 
     # st.write('HOLA')
     # Example DataFrames
-    df1 = pd.DataFrame({'A': [1, 2, 4], 'B': [4, 5, 6], 'F': [1, 2, 7]})
-    df2 = pd.DataFrame({'A': [7, 1, 4], 'B': [7, 4, 5], 'F': [5, 5, 5]})  # Assume this is your other DataFrame
+    # df1 = pd.DataFrame({'A': [1, 2, 4], 'B': [4, 5, 6], 'F': [1, 2, 7]})
+    # df2 = pd.DataFrame({'A': [7, 1, 4], 'B': [7, 4, 5], 'F': [5, 5, 5]})  # Assume this is your other DataFrame
+    df1 = pd.DataFrame({
+    'A': np.random.choice([1, 2, 3, 4, 5], 20),
+    'B': np.random.choice([4, 5, 6, 7, 8], 20),
+    'F': np.random.choice([1, 2, 3, 7, 9], 20)
+    })
+    
+    df2 = pd.DataFrame({
+        'C': np.random.choice([7, 1, 4, 2, 5], 20),
+        'D': np.random.choice([7, 4, 5, 6, 8], 20),
+        'E': np.random.choice([5, 6, 7, 8, 9], 20)
+    })
     st.dataframe(df1)
     st.dataframe(df2)
 

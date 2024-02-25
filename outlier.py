cdd353aaf8f2396e408db0cce56a1db4d2907f89
JoanClearPeaks <138578111+JoanClearPeaks@users.noreply.github.com>
2024-02-25 20:44:29 +0100

    Update outlier.py

--- outlier.py
+++ outlier.py
@@ -553,7 +553,7 @@ Select the grouping criterion for observations, where numerical values will be a
     # st.write('HOLA')
     # Example DataFrames
     df1 = pd.DataFrame({'A': [4, 2, 4], 'B': [6, 5, 6]})
-    df2 = pd.DataFrame({'C': [7, 4, 4], 'D': [7, 5, 5]})  # Assume this is your other DataFrame
+    df2 = pd.DataFrame({'C': [7, 4, 4], 'D': [7, 6, 5]})  # Assume this is your other DataFrame
     st.dataframe(df1)
     st.dataframe(df2)
     # Find the index in df1 where df2 contains two equal values in columns 'C' and 'D'

aca400340ad40850729f908fcc0559f0cccb9016
JoanClearPeaks <138578111+JoanClearPeaks@users.noreply.github.com>
2024-02-25 21:13:06 +0100

    Update outlier.py

--- outlier.py
+++ outlier.py
@@ -558,11 +558,12 @@ Select the grouping criterion for observations, where numerical values will be a
     st.dataframe(df1)
     st.dataframe(df2)
 
-    merged_df = pd.merge(df1, df2, on=['A', 'B'])
-    indices = merged_df.index.tolist()
+    mask = df1[['A', 'B']].isin(df2_copy[['A', 'B']]).all(axis=1)
+    indices = df1[mask].index.tolist()
     st.dataframe(merged_df)
     st.write(indices)
-
+    
+    merged_df = pd.merge(df1, df2, on=['A', 'B'])
     indices = merged_df['original_index'].tolist()
     st.write(indices)
     return

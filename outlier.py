a0aac60fd640ce227ad80b76f3bf2a6320f07a2b
JoanClearPeaks <138578111+JoanClearPeaks@users.noreply.github.com>
2024-03-04 10:13:32 +0100

    Update outlier.py

--- outlier.py
+++ outlier.py
@@ -82,6 +82,8 @@ class Outlier_Quantiles():
       selection[self.date_column] = selection[self.date_column].replace('2019-09-24', '2019-09-23')
       selection.iloc[1] = ['2020-06-20', 303, 300, 1200, 50, 120, 0, 0, 0, 0, 0, 0]
       selection.iloc[30] = ['2020-06-20', 303, 300, 1200, 50, 120, 0, 0, 0, 0, 0, 0]
+      selection.iloc[35] = ['2020-06-20', 298, 300, 1200, 50, 120, 0, 0, 0, 0, 0, 0]
+
       selection[self.date_column] = selection[self.date_column].dt.date
       with st.expander('DATA CHANGE'):
         st.dataframe(selection[self.date_column])

954a72169a9618f2073fd88299868c192cf36151
JoanClearPeaks <138578111+JoanClearPeaks@users.noreply.github.com>
2024-03-08 11:59:02 +0100

    Update outlier.py

--- outlier.py
+++ outlier.py
@@ -376,10 +376,11 @@ Select the grouping criterion for observations, where numerical values will be a
                   self.df_outliers = copy_selection.loc[indices]
 
                   for index, row in self.df_outliers.iterrows(): 
-                      st.write(f"Row {index}, dated {row[self.date_column]}, with a value of {row[self.target_column]} in the target column {self.target_column}. The sensitivity was ({self.sensitivity}) and the threshold range ({self.lower_threshold} - {self.upper_threshold}) has been crossed.")
-          else:
+                      st.write(f"Row {index}, dated {row[self.date_column]}, with a value of {row[self.target_column]} in the target column {self.target_column}. The sensitivity was {self.sensitivity} and the threshold range ({self.lower_threshold} - {self.upper_threshold}) has been crossed.")
+          elif self.rolling_period == 1:
                for index, row in self.df_outliers.iterrows(): 
-                      st.write(f"Row {index}, with a value of {row[self.target_column]} in the target column {self.target_column}. The sensitivity was ({self.sensitivity}) and the threshold range ({self.lower_threshold} - {self.upper_threshold}) has been crossed.")
+                    st.write(f"Row {index}, with a value of {row[self.target_column]} in the target column {self.target_column}. The sensitivity was {self.sensitivity} and the threshold range ({self.lower_threshold} - {self.upper_threshold}) has been crossed.")
+        
             # Muestra las filas seleccionadas
             # st.dataframe(filas_seleccionadas)
           
